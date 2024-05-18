d7da9d96a0999cc08acfa06a91380b43f6d06554
Zach Lagden <zach@zachlagden.uk>
2024-05-18 07:40:10 +0000

    :recycle: Refactor message tracking commands for improved code organization and error handling

--- cogs/utils/cmds_message_track.py
+++ cogs/utils/cmds_message_track.py
@@ -16,10 +16,12 @@ import discord
 from db import messages_collection
 
 # Helper functions
+from helpers.colors import MAIN_EMBED_COLOR, ERROR_EMBED_COLOR, SUCCESS_EMBED_COLOR
 from helpers.logs import RICKLOG_BG
+from helpers.errors import handle_error
 
 
-class MessagesCountCog(commands.Cog):
+class Utils_MessageTrackingCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -29,7 +31,7 @@ class MessagesCountCog(commands.Cog):
             40000: 1227251655704055891,
             200000: 1227254873037344840,
         }
-        
+
         self.reverse_role_rewards = {v: k for k, v in self.role_rewards.items()}
 
     @staticmethod
@@ -69,34 +71,41 @@ class MessagesCountCog(commands.Cog):
         sorted_roles = sorted(
             self.role_rewards.items(), key=lambda item: item[0], reverse=True
         )
-        
+
         highest_role_that_can_be_awarded = None
         for role_threshold, role_id in sorted_roles:
             if user_logger["count"] >= role_threshold:
                 highest_role_that_can_be_awarded = role_id
                 break
-        
+
         # Get all the roles the user currently has that are in the role_rewards dictionary
         current_role = None
         for role in message.author.roles:
             if role.id in self.role_rewards.values():
                 current_role = role
                 break
-        
+
         # Assign the new role if necessary
-        if highest_role_that_can_be_awarded and (current_role is None or current_role.id != highest_role_that_can_be_awarded):
+        if highest_role_that_can_be_awarded and (
+            current_role is None or current_role.id != highest_role_that_can_be_awarded
+        ):
             new_role = message.guild.get_role(highest_role_that_can_be_awarded)
             if new_role:
                 # Before assigning the new role, remove all other roles that are in the role_rewards dictionary
                 for role in message.author.roles:
                     if role.id in self.role_rewards.values():
-                        await message.author.remove_roles(role, reason="New role awarded.")
-                
-                await message.author.add_roles(new_role)
-                await message.channel.send(f"Congratulations! You have reached {user_logger['count']} messages and have been awarded the {new_role.name} role.", mention_author=False)
+                        await message.author.remove_roles(
+                            role, reason="New role awarded."
+                        )
 
-    @commands.command()
-    async def count(self, ctx: commands.Context, member: Union[discord.Member, None]):
+                await message.author.add_roles(new_role)
+                await message.channel.send(
+                    f"Congratulations! You have reached {user_logger['count']} messages and have been awarded the {new_role.name} role.",
+                    mention_author=False,
+                )
+
+    @commands.command(name="count")
+    async def _count(self, ctx: commands.Context, member: Union[discord.Member, None]):
         """
         Check the number of messages a user has sent.
         """
@@ -110,19 +119,39 @@ class MessagesCountCog(commands.Cog):
             embed = discord.Embed(
                 title=f"Messages sent by {member}",
                 description="0 messages",
-                color=discord.Color.green(),
+                color=MAIN_EMBED_COLOR,
             )
+            embed.set_footer(text="Better Hood Utils")
+
             await ctx.reply(embed=embed, mention_author=False)
         else:
             embed = discord.Embed(
                 title=f"Messages sent by {member}",
                 description=f"{user_logger['count']} messages",
-                color=discord.Color.green(),
+                color=MAIN_EMBED_COLOR,
             )
+            embed.set_footer(text="Better Hood Utils")
+
             await ctx.reply(embed=embed, mention_author=False)
 
-    @commands.command()
-    async def leaderboard(self, ctx: commands.Context):
+    @_count.error
+    async def _count_error(self, ctx: commands.Context, error):
+        if isinstance(error, commands.BadArgument):
+            embed = discord.Embed(
+                title="Invalid Usage",
+                description="Please enter a valid user.",
+                color=ERROR_EMBED_COLOR,
+            )
+            embed.add_field("Usage", value=f"```{ctx.prefix}count <user>```")
+            embed.set_footer(text="Better Hood Utils")
+
+            await ctx.reply(embed=embed, mention_author=False)
+
+        else:
+            await handle_error(ctx, error)
+
+    @commands.command(name="leaderboard")
+    async def _leaderboard(self, ctx: commands.Context):
         """
         Show the top 10 users with the most messages sent.
         """
@@ -145,5 +174,6 @@ class MessagesCountCog(commands.Cog):
 
         await ctx.reply(embed=embed, mention_author=False)
 
+
 async def setup(bot: commands.Bot):
-    await bot.add_cog(MessagesCountCog(bot))
+    await bot.add_cog(Utils_MessageTrackingCommands(bot))
